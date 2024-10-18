a071e496db071e1754c8c1110b6f37c56308f845
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-10-18 11:46:07 +0530

    Initial commit

---
+++ src/main/java/com/password/encryptfile/EncryptfileApplication.java
@@ -0,0 +1,13 @@
+package com.password.encryptfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/password/encryptfile/EncryptfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.password.encryptfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
